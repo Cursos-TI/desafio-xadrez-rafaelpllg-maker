4d834c019ec292155847bae91aa2885ff22c2460
Rafael Pellegrino <rafael.pllg@gmail.com>
2026-05-23 23:40:11 +0000

    Seguindo o enunciado do desafio, implementei o movimento do Bispo, da Torre e da Rainha usando as estruturas de repetição (while, for e do... while).

--- xadrez.c
+++ xadrez.c
@@ -7,16 +7,33 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int movimento_torre = 5, movimento_rainha = 5, movimento_bispo = 5;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    printf("\n\nMovimento Bispo\n\n");
+    while (movimento_bispo > 0)
+    {
+        printf("Frente\n");
+        printf("Esquerda\n");
+        movimento_bispo--;
+    }
+    
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("\n\nMovimento Torre\n\n");
+    for (int i = 0; i < movimento_torre; i++)
+    {
+        printf("Direita\n");
+    }
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    printf("\n\nMovimento Rainha\n\n");
+    do {
+        printf("Esquerda\n");
+        movimento_rainha--;
+    } while (movimento_rainha > 0);
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
